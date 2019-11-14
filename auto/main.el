39b63a1ae2b366fcaa06d3cd789ae190daa7da46
Douglas de Araujo Smigly <42769840+dsmigly@users.noreply.github.com>
2019-11-14 17:24:05 -0300

    +P3 e mais exercícios

--- auto/main.el
+++ auto/main.el
@@ -4,15 +4,15 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("book" "11pt" "twoside" "a4paper")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "book"
@@ -22,15 +22,29 @@
     "sec:intro"
     "new-question"
     "tab:table1"
+    "lideford"
     "lidef"
     "existbase"
+    "basefin1"
+    "basefin2"
     "unotsubsp"
+    "basedual"
+    "diagonal"
+    "autoespaco"
     "primeira"
     "segunda"
-    "terceira")
+    "terceira"
+    "diagonal1"
+    "ciclico"
+    "cayley-hamilton"
+    "cayley-hamilton-avesso"
+    "primaria minimal"
+    "primaria caracteristico"
+    "diagonal2")
    (LaTeX-add-bibliographies
     "samples")
    (LaTeX-add-index-entries
+    "Espaço Vetorial!Base ordenada"
     "Espaço Vetorial!Base"
     "Teorema de Cantor-Bernstein"
     "Espaço Vetorial!Dimensão"
@@ -38,6 +52,10 @@
     "Espaço Vetorial!Transformações Lineares"
     "Espaço Vetorial!Teorema do Núcleo-Imagem"
     "Transformações Lineares!Isomorfismos"
-    "Espaço Vetorial!Base dual"))
+    "Espaço Vetorial!Base dual"
+    "Autovalor"
+    "Autovetor"
+    "Autoespaço"
+    "Spectrum"))
  :latex)
 
